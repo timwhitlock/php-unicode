6f2757961ffe15855e22210656bb79d0ab12ead1
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-11-22 14:42:40 +0000

    added ::codes test

--- test/tests/Utf8String/CodesTest.php
+++ test/tests/Utf8String/CodesTest.php
@@ -17,4 +17,28 @@ class CodesTest extends \PHPUnit_Framework_TestCase {
         }
     }
 
+    public function testHighAsciiMatchesPhpUtf8Decode(){
+        for( $i = 0x80; $i <= 0xFF; $i++ ){
+            $singlebyte = chr($i);
+            $multibyte = utf8_encode($singlebyte);
+            $codes = Utf8String::codes( $multibyte );
+            $this->assertEquals( array($i), $codes );
+        }
+    }
+
+
+    public function testDoubleByte(){
+        $this->assertEquals( array( 0xA3, 0x20AC ), Utf8String::codes('£€') );
+    }
+
+
+    public function testTripleBytes(){
+        $this->assertEquals( array( 0x2714, 0x2715 ), Utf8String::codes('✔✕') );
+    }
+    
+    public function testQuadrupleByte(){
+        $this->assertEquals( array( 0x1F601 ), Utf8String::codes("\xF0\x9F\x98\x81") );
+    }
+
+
 }
