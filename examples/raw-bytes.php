516c69408c383f9e07d559bcdf1c2288adbb3102
Tim Whitlock <r-bitbucket@timwhitlock.info>
2014-11-22 13:46:09 +0000

    last commit before legacy branch

--- examples/raw-bytes.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf8', true );
-echo "\xE2\x9C\x94";
--- examples/u-function.php
+++ examples/u-function.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Print tick mark using the u() syntax
+ */
+
+chdir(__DIR__);
 require('../php-unicode.php');
+
 header('Content-Type: text/html; charset=utf8', true );
-echo u('\u2714');
+
+echo u('\u2714'),"\n";
